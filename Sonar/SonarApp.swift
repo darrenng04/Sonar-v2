8b26473edb85e014cec2a7782d181a2ba9ebf469
Darren Ng <darrenng1704@gmail.com>
2026-05-28 21:34:42 -0500

    Initial Commit

---
+++ Sonar/SonarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarApp.swift
+//  Sonar
+//
+//  Created by Darren Ng on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SonarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SonarTests/SonarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SonarTests.swift
+//  SonarTests
+//
+//  Created by Darren Ng on 5/28/26.
+//
+
+import Testing
+@testable import Sonar
+
+struct SonarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
